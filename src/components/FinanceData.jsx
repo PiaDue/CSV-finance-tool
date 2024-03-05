3c987bbcd68a45c9baca2ecc0aa014edb5a4a5ce
PiaDue <pia.duenow@web.de>
2024-03-05 19:31:49 +0100

    skip lines

--- src/components/FinanceData.jsx
+++ src/components/FinanceData.jsx
@@ -1,34 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useFile } from '../contexts/FileContext';
 
 function FinanceData() {
     const { parsedData } = useFile();
+    const [skipLines, setSkipLines] = useState(4);
+
+    useEffect(() => {
+        console.log(parsedData);
+    }, [parsedData]);
+
+    if (!parsedData) {
+        return <div>No data available</div>;
+    }
 
     return (
         <>
             {parsedData && (
                 <div>
-                    <h2>Parsed Data:</h2>
-                    <div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    {parsedData[0].map((cell, index) => (
-                                        <th key={index}>{cell}</th>
+                    <div className='lineSkipper'>
+                        <label htmlFor="skipLinesInput">Skip Lines:</label>
+                        <input
+                            id="skipLinesInput"
+                            type="number"
+                            value={skipLines}
+                            onChange={() => { setSkipLines(parseInt(event.target.value)); }}
+                            min={0}
+                        />
+                    </div>
+                    <table className='table table-hover'>
+                        <thead>
+                            <tr>
+                                <th scope="col">*</th>
+                                {parsedData[skipLines].map((cell, index) => (
+                                    <th key={index} scope="col">{cell}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {parsedData.slice(skipLines + 1).map((row, index) => (
+                                <tr key={index}>
+                                    <th scope="row">{index + 1}</th>
+                                    {row.map((cell, index) => (
+                                        <td key={index}>{cell}</td>
                                     ))}
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {parsedData.slice(1).map((row, index) => (
-                                    <tr key={index}>
-                                        {row.map((cell, index) => (
-                                            <td key={index}>{cell}</td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             )}
         </>
