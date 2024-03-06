b6eab3b718a0abce6fa3d0bec57da35a91d197f6
PiaDue <pia.duenow@web.de>
2024-03-06 23:50:02 +0100

    exclude unimportant columns

--- src/components/FinanceData.jsx
+++ src/components/FinanceData.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useFile } from '../contexts/FileContext';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function FinanceData() {
     const { parsedData } = useFile();
     const [skipLines, setSkipLines] = useState(4);
+    const excludedColumns = [1,2,6,7,9,10,11]; //TODO: make this dynamic
 
     useEffect(() => {
         console.log(parsedData);
@@ -32,7 +34,7 @@ function FinanceData() {
                             <tr>
                                 <th scope="col">*</th>
                                 {parsedData[skipLines].map((cell, index) => (
-                                    <th key={index} scope="col">{cell}</th>
+                                    !excludedColumns.includes(index) && <th key={index} scope="col">{cell}</th>
                                 ))}
                             </tr>
                         </thead>
@@ -41,12 +43,15 @@ function FinanceData() {
                                 <tr key={index}>
                                     <th scope="row">{index + 1}</th>
                                     {row.map((cell, index) => (
-                                        <td key={index}>{cell}</td>
+                                        !excludedColumns.includes(index) && <td key={index}>{cell}</td>
                                     ))}
                                 </tr>
                             ))}
                         </tbody>
                     </table>
+                    <button className="btn btn-primary btn-lg position-fixed bottom-0 end-0 m-3" onClick={() => { console.log('Analyze'); }}>
+                        Analyze
+                    </button>
                 </div>
             )}
         </>
